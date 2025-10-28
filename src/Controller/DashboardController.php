67bf188c87874eb393a762b166412affc9a6bbf3
Idris jose <idrisjose11@gmail.com>
2025-10-28 20:17:04 +0100

    new changes made

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -30,6 +30,24 @@ class DashboardController
         $content = $this->twig->render('pages/dashboard.twig', [
             'user' => $user,
             'stats' => $stats,
+            'chartData' => json_encode([
+                'labels' => ['Open', 'In Progress', 'Closed'],
+                'datasets' => [[
+                    'label' => 'Tickets',
+                    'data' => [$stats['open'], $stats['in_progress'], $stats['closed']],
+                    'backgroundColor' => [
+                        'rgba(34, 197, 94, 0.8)', // green
+                        'rgba(245, 158, 11, 0.8)', // amber
+                        'rgba(107, 114, 128, 0.8)', // gray
+                    ],
+                    'borderColor' => [
+                        'rgb(34, 197, 94)',
+                        'rgb(245, 158, 11)',
+                        'rgb(107, 114, 128)',
+                    ],
+                    'borderWidth' => 1
+                ]]
+            ]),
         ]);
 
         return new Response($content);
